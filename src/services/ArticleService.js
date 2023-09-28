9ad75aad52708c35133ef7f9c92a51695bb244fe
Jimmy <jimmysch123@gmail.com>
2023-09-28 13:19:41 +0200

    Update ArticleService.js

--- src/services/ArticleService.js
+++ src/services/ArticleService.js
@@ -3,10 +3,10 @@ const api = 'https://localhost:7119/api';
 
 export async function GetAllArticles(){
     try {
-        // console.log("service getAllArticles");
+        console.log("service getAllArticles");
         const response = await axios.get(`${api}/Article`
             );
-        // console.log("response", response);
+        console.log("response", response);
         return response.data;
     } catch (error) {
         return [];
@@ -17,21 +17,17 @@ export async function CreateArticle(data){
     try {
         const response = axios.post(`${api}/Article`, data)
         .catch(function (error) {
-            if (error.response) {
                 // Request made and server responded
-                // console.log(error.response.data);
-                // console.log(error.response.status);
-                // console.log(error.response.headers);
-            } else if (error.request) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
                 // The request was made but no response was received
-                // console.log(error.request);
-              } else {
+                console.log(error.request);
                 // Something happened in setting up the request that triggered an Error
-                // console.log('Error', error.message);
-              }
+                console.log('Error', error.message);
         });
         return response;
     } catch (error) {
-        // console.log(error);
+        console.log(error);
     }
 }
