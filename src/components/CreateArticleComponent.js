05de38ac9fa0c114e63a95865af5bb1124118295
Jimmy <jimmysch123@gmail.com>
2023-10-12 14:12:05 +0200

    Fixed embedding

--- src/components/CreateArticleComponent.js
+++ src/components/CreateArticleComponent.js
@@ -1,44 +1,183 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { CreateArticle, GetAllArticles } from "../services/ArticleService";
+import TextToLinksConverter from "./EmbedComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function Embed({ content }) {
-  if (content.startsWith('http')) {
-    if (content.match(/\.(jpg|jpeg|png|gif|bmp)$/)) {
-      // Handle image links
-      const imageStyle = 'max-width: 60%; max-height: 30vh; height: auto; width: auto;';
-      return <img className="responsive-image" src={content} alt="Image" style={imageStyle} />;
-    } else if (content.includes('youtube.com/watch') || content.includes('youtu.be/')) {
-      // Handle YouTube links
-      const youtubeId = getYouTubeId(content);
-      if (youtubeId) {
-        return (
-          <div className="video-responsive">
-            <iframe
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${youtubeId}`}
-              frameBorder="0"
-              allowFullScreen
-              title="Embedded Video"
-            />
-          </div>
-        );
+const CreateArticleComponent = () => {
+  const [articles, setArticles] = useState(null);
+  var htmlArticleList;
+  var htmlMakeArticleName;
+  var htmlMakeArticleBody;
+
+  useEffect(() => {
+    async function AsignValue() {
+      setArticles(await GetAllArticleTitles());
+    }
+    AsignValue();
+  }, []);
+  useEffect(() => {
+    htmlArticleList = document.getElementById("articleList");
+    htmlMakeArticleName = document.getElementById("makearticlenames");
+    htmlMakeArticleBody = document.getElementById("makearticlebody");
+  }, []);
+
+  async function PostArticle() {
+    let articleName = htmlMakeArticleName.value;
+    let articleBody = htmlMakeArticleBody.value;
+    if (articleName <= 0) {
+      console.log("article name can't be empty");
+      ChangeMessage(2);
+      return;
+    }
+
+    let articlebody = {
+      Title: articleName,
+      Body: articleBody,
+    };
+
+    try {
+      await CreateArticle(articlebody).then((response) => {
+        if (response.id !== null) {
+          console.log("Succesfully created a article");
+          toastMessage("Succes", "Succesfully created a game.");
+          GetAllArticleTitles();
+        } else {
+          console.log("Failed to create a game");
+        }
+      });
+    } catch (error) {}
+  }
+
+  const SetArticleList = (Articles) => {
+    htmlArticleList.innerHTML = "Loading data";
+  
+    Articles.forEach(element => {
+      const articleTitle = element.title;
+      let articleBody = element.body;
+  
+      if (articleBody) {
+        articleBody = TextToLinksConverter(articleBody);
       }
+  
+      htmlArticleList.innerHTML += `
+        <tr>
+          <td>${articleTitle}</td>
+          <td>${articleBody || ''}</td>
+        </tr>
+      `;
+    });
+  };
+  
+  
+
+  async function GetAllArticleTitles() {
+    var articleTitles = [];
+    await GetAllArticles().then((response) => {
+      articleTitles = response;
+      SetArticleList(articleTitles);
+    });
+  }
+
+  function ChangeMessage(typeMessage) {
+    switch (typeMessage) {
+      case 0:
+        toastMessage("error", "Rounds can't be negative.");
+        break;
+      case 1:
+        toastMessage("error", "Rounds can't be over 10.");
+        break;
+      case 2:
+        toastMessage("error", "Players can't be less then 2.");
+        break;
+      case 3:
+        toastMessage("Succes", "Succesfully created a game.");
+        break;
+      default:
+        console.log("There isn't a switch case for this", typeMessage);
     }
   }
 
-  // Handle regular links
+  async function toastMessage(type, message) {
+    if (type === "error") {
+      toast.error(message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    } else {
+      toast.success(message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
+  }
   return (
-    <a href={content} target="_blank" rel="noopener noreferrer">
-      {content}
-    </a>
+    <>
+      <div className="input-group mb-3">
+        <span className="input-group-text" id="inputGroup-sizing-default">
+          Make article names
+        </span>
+        <input
+          id="makearticlenames"
+          data-testid="CAC-MakeArticleNames"
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-default"
+        />
+      </div>
+      <div className="input-group mb-3">
+        <span className="input-group-text" id="inputGroup-sizing-default">
+          Make article body
+        </span>
+        <input
+          id="makearticlebody"
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-default"
+        />
+      </div>
+
+      <div>
+        <div
+          className="btn btn-info"
+          data-testid="CAC-MakeArticleButton"
+          onClick={PostArticle}
+        >
+          Make articles
+        </div>
+      </div>
+
+      <div className="container">
+        <h3 className="p-3 text-center" data-testid="CAC-ArticleNames">
+          Article Names
+        </h3>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Articles</th>
+            </tr>
+          </thead>
+          <tbody id="articleList" data-testid="CAC-ArticleList"></tbody>
+        </table>
+      </div>
+    </>
   );
-}
+};
 
-function getYouTubeId(link) {
-  // Extract the YouTube video ID from the link
-  const youtubeIdRegex = /[?&]v=([A-Za-z0-9_\-]+)/;
-  const match = link.match(youtubeIdRegex);
-  return match ? match[1] : null;
-}
-
-export default Embed;
+export default CreateArticleComponent;
---
+++ src/components/EmbedComponent.js
@@ -0,0 +1,29 @@
+const TextToLinksConverter = (text) => {
+  if (!text) {
+    return null;
+  }
+
+  const urlRegex = /(https?:\/\/[^\s/$.?#].[^\s]*)/g;
+
+  const parts = text.split(urlRegex);
+
+  return parts.map((part, index) => {
+    if (urlRegex.test(part)) {
+      if (part.match(/\.(jpeg|jpg|gif|png|webp|bmp|svg)$/)) {
+        // Image links
+        return `<img src="${part}" alt="Image" style="max-width: 50%; max-height: 25%" />`;
+      } else if (part.includes('youtube.com/watch?v=')) {
+        // YouTube video links with doubled width and height
+        const videoId = part.split('v=')[1];
+        return `<div class="video-container"><iframe width="640" height="360" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe></div>`;
+      } else {
+        // Other links
+        return `<a href="${part}" target="_blank" rel="noreferrer">${part}</a>`;
+      }
+    } else {
+      return part;
+    }
+  }).join('');
+};
+
+export default TextToLinksConverter;
